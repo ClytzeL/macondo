b7dcd8a85c446c4d31d78c94abafde71b869933c
macondo <1512434854@qq.com>
2023-12-21 09:42:15 +0800

    feat:fix

--- src/app/page-copy.tsx
+++ src/app/page-copy.tsx
@@ -3,37 +3,41 @@ import { getServerSession } from "next-auth/next";
 import { authOptions } from "@/server/auth";
 import Image from "next/image";
 import Form from "./form";
-import { prisma } from "@/server/db";
+// import { prisma } from "@/server/db";
 import Delete from "./delete";
 import { type Session } from "next-auth";
 
-async function getPosts() {
-  const data = await prisma.posts.findMany({
-    include: {
-      author: true,
-    },
-    orderBy: {
-      createdAt: "desc",
-    },
-  });
-  return data;
-}
+// async function getPosts() {
+//   const data = await prisma.posts.findMany({
+//     include: {
+//       author: true,
+//     },
+//     orderBy: {
+//       createdAt: "desc",
+//     },
+//   });
+//   return data;
+// }
 
 export default async function Home() {
   let session: Session | null | undefined;
   let posts;
 
   try {
-    const [postsRes, sessionRes] = await Promise.allSettled([
-      getPosts(),
+    // const [postsRes, sessionRes] = await Promise.allSettled([
+    //   getPosts(), 
+    //   getServerSession(authOptions),
+    // ]);
+    const [sessionRes] = await Promise.allSettled([
+      // getPosts(), 
       getServerSession(authOptions),
     ]);
 
-    if (postsRes.status === "fulfilled" && postsRes.value[0]) {
-      posts = postsRes.value;
-    } else {
-      console.error(postsRes);
-    }
+    // if (postsRes.status === "fulfilled" && postsRes.value[0]) {
+    //   posts = postsRes.value;
+    // } else {
+    //   console.error(postsRes);
+    // }
 
     if (sessionRes.status === "fulfilled") {
       session = sessionRes.value;
@@ -76,7 +80,7 @@ export default async function Home() {
             )}
           </div>
           <div className="flex max-w-md flex-col items-center justify-center gap-5">
-            {posts?.map((post) => (
+            {/* {posts?.map((post) => (
               <div
                 key={post.id}
                 className="flex flex-row items-center justify-center gap-2"
@@ -87,7 +91,7 @@ export default async function Home() {
                   <Delete id={post.id} />
                 )}
               </div>
-            ))}
+            ))} */}
           </div>
         </div>
       </main>
